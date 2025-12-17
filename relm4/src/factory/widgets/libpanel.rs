44b369a27d428cd9740ce3b68aae5bb56af7fea1
LlamasHere <llamashere@posteo.de>
2025-12-17 20:17:57 +0200

    Implement factory trait for libpanel Document Workspace

--- relm4/src/factory/widgets/libpanel.rs
+++ relm4/src/factory/widgets/libpanel.rs
@@ -41,3 +41,52 @@ impl FactoryView for panel::Paned {
         self.insert(0, widget);
     }
 }
+
+impl FactoryView for panel::DocumentWorkspace {
+    type Children = gtk::Widget;
+    type ReturnedWidget = panel::Widget;
+    type Position = panel::Area;
+
+    fn factory_remove(&self, widget: &Self::ReturnedWidget) {
+        widget.close();
+    }
+
+    fn factory_append(
+        &self,
+        child: impl AsRef<Self::Children>,
+        position: &Self::Position,
+    ) -> Self::ReturnedWidget {
+        let widget = panel::Widget::new();
+        widget.set_child(Some(child.as_ref()));
+        let w_pos = panel::Position::builder().area(*position).build();
+        self.add_widget(&widget, Some(&w_pos));
+        widget
+    }
+
+    fn factory_prepend(
+        &self,
+        widget: impl AsRef<Self::Children>,
+        position: &Self::Position,
+    ) -> Self::ReturnedWidget {
+        self.factory_append(widget, position)
+    }
+
+    fn factory_insert_after(
+        &self,
+        widget: impl AsRef<Self::Children>,
+        position: &Self::Position,
+        _other: &Self::ReturnedWidget,
+    ) -> Self::ReturnedWidget {
+        self.factory_append(widget, position)
+    }
+
+    fn returned_widget_to_child(root_child: &Self::ReturnedWidget) -> Self::Children {
+        root_child
+            .child()
+            .expect("That panel widget has no child????")
+    }
+
+    fn factory_move_after(&self, _widget: &Self::ReturnedWidget, _other: &Self::ReturnedWidget) {}
+
+    fn factory_move_start(&self, _widget: &Self::ReturnedWidget) {}
+}
